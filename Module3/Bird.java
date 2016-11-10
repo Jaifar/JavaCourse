9731e1c5d0e7f731c56d369663883c8ac1c329df
Jaifar <tetiana.kravchuk1608@gmail.com>
2016-11-10 13:41:33 -0500

    BirdSing

---
+++ Module3/Bird.java
@@ -0,0 +1,20 @@
+package Module3;
+
+/**
+ * Created by Owner on 11/9/2016.
+ */
+public class Bird {
+  public static void main(String[] args){
+
+
+  // make a Bird object and access it
+  Bird bird1 = new Bird();
+        bird1.sing();}
+
+    public void sing(){
+      System.out.println("I am walking");
+      System.out.println("I am flying");
+      System.out.println("I am singing");
+      System.out.println("I am Bird");
+    }
+}
