33693c18ebc47654abefc91c149b8e741afd6a5e
Jaifar <tetiana.kravchuk1608@gmail.com>
2016-11-11 19:56:03 -0500

    3.2

---
+++ Module3/Adder.java
@@ -0,0 +1,21 @@
+package Module3;
+
+/**
+ * Created by Owner on 11/10/2016.
+ */
+public class Adder extends Arithmetic {
+  public static void main(String[] args) {
+    int num1 = 2;
+    int num2 = 3;
+
+    if (num1 >= num2) {
+      System.out.println("True");
+    } else {
+      System.out.println("False");
+    }
+
+
+  }
+}
+
+
---
+++ Module3/Arithmetic.java
@@ -0,0 +1,18 @@
+package Module3;
+
+/**
+ * Created by Owner on 11/10/2016.
+ */
+public class Arithmetic {
+  // private = local to this class
+  private static int addIntegers(int a, int b)
+  {
+    return (a+b);
+  }
+
+
+
+
+
+  }
+
