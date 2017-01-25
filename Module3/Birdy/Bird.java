8eb5ab75342f716091c83d436a1a2c1c618d32e8
Jaifar <tetiana.kravchuk1608@gmail.com>
2017-01-25 13:56:22 -0500

    Birdy 3.1

--- Module3/Birdy/Bird.java
+++ Module3/Birdy/Bird.java
@@ -3,12 +3,22 @@ package Module3.Birdy;
 /**
  * Created by Owner on 11/9/2016.
  */
-public class Bird  extends BirdSinging{
-  
-     void sing(){
-      System.out.println("I am walking");
-      System.out.println("I am flying");
-      System.out.println("I am singing");
-      System.out.println("I am Bird");
-    }
-}
+public class Bird {
+  public String bird;
+
+
+  public void sing(){
+    System.out.println("I am singing");}
+
+  public void fly(){
+    System.out.println("I am flying");
+  }
+  public void say(){
+    System.out.println("I am a Bird");
+
+  }
+  public void walk(){
+    System.out.println("I am walking");
+
+  }
+  }
---
+++ Module3/Birdy/MainBird.java
@@ -0,0 +1,16 @@
+package Module3.Birdy;
+
+/**
+ * Created by Owner on 12/29/2016.
+ */
+public class MainBird {
+  public static void main(String[] args) {
+
+    Bird bird= new Bird();
+    bird.sing();
+    bird.say();
+    bird.walk();
+    bird.fly();
+
+  }
+}
