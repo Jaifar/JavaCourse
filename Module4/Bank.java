6a3e455006e9c0e8d6491a7aa39514635fc1f963
Jaifar <tetiana.kravchuk1608@gmail.com>
2017-01-31 21:49:40 -0500

    Abstract Bank

--- Module4/Bank.java
+++ Module4/Bank.java
@@ -1,91 +1,113 @@
 package Module4;
 
-import static Module4.Currency.*;
+import java.util.Currency;
 
 /**
- * Created by Owner on 11/29/2016.
+ * Created by Owner on 1/27/2017.
  */
-abstract public class Bank  {
-// the the attributes
+public abstract class Bank {
+  //Создайте следующую структуру:
+  /////1. Абстрактный класс Bank и 3 класса, которые его расширяют: USBank, EUBank, ChinaBank.
+
+  //Абстрактный класс Bank должен содержать поля:
+  //long id
+  //String bankCountry
+  //Currency currency
+  //int numberOfEmployees
+  //double avrSalaryOfEmployee
+  //long rating
+  //long totalCapital
+
+  //Создайте get-,set-методы и конструктор с аргументами - всеми полями.
+  //Методы:
+//abstract int getLimitOfWithdrawal()
+//abstract int getLimitOfFunding()
+//abstract int getMonthlyRate()
+//abstract int getCommission(int summ)
+  //       double moneyPaidMonthlyForSalary()
   private long id;
   private String bankCountry;
-
+  private Currency currency;
   private int numberOfEmployees;
-  double avrSalaryOfEmployee;
-  long rating;
-  long totalCapital;
-  private int limitWithdraw;
-  private int limitDeposit;
-  private int monthComission;
-  String currency;
-  // the methods
-
-  // constructor
-  public Bank (long id, String bankCountry, Currency currency,int numberOfEmployees ){
+  private double avrSalaryOfEmployee;
+  private long rating;
+  private long totalCapital;
 
 
-
+  private Bank(long id, String bankCountry, Currency currency, int numberOfEmployees, double avrSalaryOfEmployee, long rating, long totalCapital) {
+    this.id = id;
+    this.bankCountry = bankCountry;
+    this.currency = currency;
+    this.numberOfEmployees = numberOfEmployees;
+    this.avrSalaryOfEmployee = avrSalaryOfEmployee;
+    this.rating = rating;
+    this.totalCapital = totalCapital;
   }
-  // methods to read the attributes
-  public void deposit(double amount){
 
-  }
-  public void withdraw (double amount){
-
-  }
-  public long getId() {
+  private long getId() {
     return id;
   }
 
-  public void setId(long id) {
+  private void setId(long id) {
     this.id = id;
   }
 
-  public String getBankCountry() {
+  private String getBankCountry() {
     return bankCountry;
   }
 
-  public void setBankCountry(String bankCountry) {
+  private void setBankCountry(String bankCountry) {
     this.bankCountry = bankCountry;
   }
 
-  public Currency getCurrency() {
+  private Currency getCurrency() {
     return currency;
   }
 
-  public void setCurrency(Currency currency) {
+  private void setCurrency(Currency currency) {
     this.currency = currency;
   }
 
-  public int getNumberOfEmployees() {
+  private int getNumberOfEmployees() {
     return numberOfEmployees;
   }
 
-  public void setNumberOfEmployees(int numberOfEmployees) {
+  private void setNumberOfEmployees(int numberOfEmployees) {
     this.numberOfEmployees = numberOfEmployees;
   }
 
-  public double getAvrSalaryOfEmployee() {
+  private double getAvrSalaryOfEmployee() {
     return avrSalaryOfEmployee;
   }
 
-  public void setAvrSalaryOfEmployee(double avrSalaryOfEmployee) {
+  private void setAvrSalaryOfEmployee(double avrSalaryOfEmployee) {
     this.avrSalaryOfEmployee = avrSalaryOfEmployee;
   }
 
-  public long getRating() {
+  private long getRating() {
     return rating;
   }
 
-  public void setRating(long rating) {
+  private void setRating(long rating) {
     this.rating = rating;
   }
 
-  public long getTotalCapital() {
+  private long getTotalCapital() {
     return totalCapital;
   }
 
-  public void setTotalCapital(long totalCapital) {
+  private void setTotalCapital(long totalCapital) {
     this.totalCapital = totalCapital;
-  }}
-// methods to deposit and withdraw money
+  }
+
+  abstract int getLimitOfWithdrawal();
+
+  abstract int getLimitOfFunding();
+
+  abstract int getMonthlyRate();
+
+  abstract int getCommission(int summ);
+
+  abstract double moneyPaidMonthlyForSalary();
+
+}
