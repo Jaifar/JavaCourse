4f091439d22230085ebb1bb11edda1a0de5e438b
Jaifar <tetiana.kravchuk1608@gmail.com>
2017-02-11 20:55:16 -0500

    redone

--- Module3/Arithmetic/Adder.java
+++ Module3/Arithmetic/Adder.java
@@ -6,11 +6,11 @@ package Module3.Arithmetic;
 public class Adder extends Arithmetic {
   // Напишите класс Adder, который наследуется от класса Arithmetic.
   // У класса Adder должен быть метод check, который сравнивает 2 номера и возвращает true, если а>=b и false в других случаях.
-  boolean check(Integer a, Integer b){
-    if (a<=b)
+  boolean check(Integer a, Integer b) {
+    if (a >= b)
       return true;
-      else
-        return false;
+    else
+      return false;
   }
 
 }
---
+++ Module3/Arithmetic/Main.java
@@ -0,0 +1,15 @@
+package Module3.Arithmetic;
+
+/**
+ * Created by Owner on 2/11/2017.
+ */
+public class Main {
+  public static void main(String[] args) {
+    Adder adder = new Adder();
+    Arithmetic arithmetic = new Arithmetic();
+    adder.check(2,3);
+    arithmetic.add(3,4);
+    adder.add(3,5);
+    
+  }
+}
