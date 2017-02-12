4b7c32f8aa627c911bfdb6b6bc17e2c6537b2772
Jaifar <tetiana.kravchuk1608@gmail.com>
2017-02-11 22:22:59 -0500

    HW4.5

--- Module4/BankSystem.java
+++ Module4/BankSystem.java
@@ -4,4 +4,9 @@ package Module4;
  * Created by Owner on 2/9/2017.
  */
 public interface BankSystem {
+
+  void withdrawOfUser(User user, int amount);
+  void fundUser(User user, int amount);
+  void transferMoney(User fromUser, User toUser, int amount);
+  void paySalary(User user);
 }
