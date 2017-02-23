5508ae9b8803e4aedc3af8e29d9dd70c4fc68e33
Tetiana Kravchuk <tetiana.kravchuk1608@gmail.com>
2017-02-22 22:32:50 -0500

    BankS

--- Module4/BankSystem.java
+++ Module4/BankSystem.java
@@ -5,8 +5,11 @@ package Module4;
  */
 public interface BankSystem {
 
-  void withdrawOfUser(User user, int amount);
-  void fundUser(User user, int amount);
-  void transferMoney(User fromUser, User toUser, int amount);
-  void paySalary(User user);
+    void withdrawOfUser(User user, int amount);
+
+    void fundUser(User user, int amount);
+
+    void transferMoney(User fromUser, User toUser, int amount);
+
+    void paySalary(User user);
 }
