52df1e0181fc71276a644f03b346ebeef88c31ac
Tetiana Kravchuk <tetiana.kravchuk1608@gmail.com>
2017-02-22 22:33:50 -0500

    BankImplm

--- Module4/BankSystemImpl.java
+++ Module4/BankSystemImpl.java
@@ -3,24 +3,24 @@ package Module4;
 /**
  * Created by Owner on 2/9/2017.
  */
-public class BankSystemImpl implements BankSystem  {
-  @Override
-  public void withdrawOfUser(User user, int amount) {
+public class BankSystemImpl implements BankSystem {
+    @Override
+    public void withdrawOfUser(User user, int amount) {
 
-  }
+    }
 
-  @Override
-  public void fundUser(User user, int amount) {
+    @Override
+    public void fundUser(User user, int amount) {
 
-  }
+    }
 
-  @Override
-  public void transferMoney(User fromUser, User toUser, int amount) {
+    @Override
+    public void transferMoney(User fromUser, User toUser, int amount) {
 
-  }
+    }
 
-  @Override
-  public void paySalary(User user) {
+    @Override
+    public void paySalary(User user) {
 
-  }
+    }
 }
