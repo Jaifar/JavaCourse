ec5e3626ba1b72d0bd4579345711c50cec105f22
Tetiana Kravchuk <tetiana.kravchuk1608@gmail.com>
2017-02-28 18:18:12 -0500

    HalfDone

--- Module5/Main.java
+++ Module5/Main.java
@@ -6,9 +6,13 @@ package Module5;
 public class Main {
     public static void main(String[] args) {
 
-    }
+        Controller controller = new Controller();
+        controller.requestRooms(34, 12, "London", "Star");
+        controller.requestRooms(34, 45, "Paris", "U");
+        controller.requestRooms(3, 4, "Dublin", "U");
+     
 //    В методе класса Main создайте Controller и сделайте 3 запроса с разными параметрами, чтобы найти номера.
 //    Также проверьте все методы 3 раза с разными API, чтобы продемонстрировать, как работает программа.
 
-Controller controller = new Controller();
-}
+
+}}
