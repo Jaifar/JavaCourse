fe74bd7b44e3f052005e5111af21221dfb7a2881
Tetiana Kravchuk <tetiana.kravchuk1608@gmail.com>
2017-03-07 10:09:53 -0500

    Задание 9

--- Module5/DAO.java
+++ Module5/DAO.java
@@ -11,4 +11,15 @@ public interface DAO {
     Room update(Room room);
 
     Room findById(long id);
+
+    Room[] getAll(); // public abstract Room[] getAll();
+
+
+//    Add method to DAO interface it and implement
+//            Room[] getAll() - return array of all rooms that are in our DB array
+//    save - adds room to arra
+//    delete - deletes rooms from array
+//    update - updates room in array (find room to update by id)
+//    find by id - find room in array
+
 }
